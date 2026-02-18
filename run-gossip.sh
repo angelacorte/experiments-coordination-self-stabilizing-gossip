2019fc80db4650f926200e71de506eae166e6883
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2026-02-18 18:53:58 +0100

    fix: date in script not working

--- run-gossip.sh
+++ run-gossip.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-DESTINATION="$HOME/Downloads/gossip-basic-experiment-$(date --utc "+%F-%H.%M.%S")"
+DESTINATION="$HOME/Downloads/gossip-basic-experiment-$(date -u "+%Y-%m-%d-%H.%M.%S")"
 git clone https://github.com/angelacorte/experiments-coordination-self-stabilizing-gossip "$DESTINATION"
 cd "$DESTINATION"
 ./gradlew runGossipGraphic
